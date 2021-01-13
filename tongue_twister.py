1ba1b13f297d050861a0781b9094497a8a7b3866
Alex <format37@gmail.com>
2021-01-13 23:14:39 +0300

    modified:   tongue_twister.py

--- tongue_twister.py
+++ tongue_twister.py
@@ -331,7 +331,9 @@ def generate_word(cur,con,user_id,in_word_id):
 		else:
 			# Выберем указанную скороговорку
 			query = "select id,word from words where id="+in_word_id+";"
-
+		# debug ++
+		print(query)
+		# debug --
 		mysql_df = pd.read_sql(query, con=con)
 		if len(mysql_df) > 0:
 			word_id = str(mysql_df.loc[0].values[0])
